1874b0295af072c52b8b9e41348c5594a727d416
GodsWithin <robin.de.zwaef@telenet.be>
2019-06-24 16:40:19 +0200

    Initial commit

---
+++ EmotePlusPlus/Program.cs
@@ -0,0 +1,70 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using EmotePlusPlus.Services;
+using LiteDB;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace EmotePlusPlus
+{
+    class Program
+    {
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            // You should dispose a service provider created using ASP.NET
+            // when you are finished using it, at the end of your app's lifetime.
+            // If you use another dependency injection framework, you should inspect
+            // its documentation for the best way to do this.
+            using (var services = ConfigureServices())
+            {
+                var client = services.GetRequiredService<DiscordSocketClient>();
+                var config = services.GetRequiredService<IConfigurationRoot>();
+
+                await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+
+                client.Log += OnLog;
+
+                // Tokens should be considered secret data and never hard-coded.
+                // We can read from the config to avoid hardcoding.
+                await client.LoginAsync(TokenType.Bot, config["token"]);
+                await client.StartAsync();
+
+                await Task.Delay(-1);
+            }
+        }
+
+        private Task OnLog(LogMessage arg)
+        {
+            Console.WriteLine(arg.Message);
+            return Task.CompletedTask;
+        }
+
+        private ServiceProvider ConfigureServices()
+        {
+            var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("config.json").Build();
+
+            return new ServiceCollection()
+                // Base
+                .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+                {
+                    LogLevel = LogSeverity.Info,
+                    AlwaysDownloadUsers = true,
+                    MessageCacheSize = 100
+                }))
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                // Config
+                .AddSingleton(config)
+                // Database
+                .AddSingleton(new LiteDatabase(new ConnectionString() { Filename = "bot.db", UtcDate = true }))
+                .BuildServiceProvider();
+        }
+    }
+}
---
+++ EmotePlusPlus/Services/CommandHandlingService.cs
@@ -0,0 +1,83 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using LiteDB;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmotePlusPlus.Services
+{
+    public class CommandHandlingService
+    {
+        private readonly DiscordSocketClient _discord;
+        private readonly CommandService _commands;
+        private readonly LiteDatabase _database;
+
+        private readonly IServiceProvider _services;
+
+        public CommandHandlingService(IServiceProvider services)
+        {
+            _discord = services.GetRequiredService<DiscordSocketClient>();
+            _commands = services.GetRequiredService<CommandService>();
+            _database = services.GetRequiredService<LiteDatabase>();
+            _services = services;
+
+            // Hook CommandExecuted to handle post-command-execution logic.
+            _commands.CommandExecuted += CommandExecutedAsync;
+            // Hook MessageReceived so we can process each message to see
+            // if it qualifies as a command.
+            _discord.MessageReceived += MessageReceivedAsync;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            // Add additional initialization code here...
+        }
+
+        public async Task MessageReceivedAsync(SocketMessage msg)
+        {
+            // Ignore system messages and messages from bots.
+            
+            if (!(msg is SocketUserMessage message)) return;
+            if (message.Source != MessageSource.User) return;
+
+            var context = new SocketCommandContext(_discord, message);
+            await context.Channel.SendMessageAsync("Ping pong");
+            int argPos = 0;
+            if (!message.HasMentionPrefix(_discord.CurrentUser, ref argPos)
+                && !message.HasCharPrefix('+', ref argPos)) return;
+
+            // Perform the execution of the command. In this method,
+            // the command service will perform precondition and parsing check
+            // then execute the command if one is matched.
+            await _commands.ExecuteAsync(context, argPos, _services);
+            // Note that normally a result will be returned by this format, but here
+            // we will handle the result in CommandExecutedAsync.
+        }
+
+        public async Task CommandExecutedAsync(Optional<CommandInfo> command, ICommandContext context, IResult result)
+        {
+            // Command is unspecified when there was a search failure (command not found); we don't care about these errors.
+            if (!command.IsSpecified)
+                return;
+
+            // The command was successful, we don't care about this result, unless we want to log that a command succeeded.
+            if (result.IsSuccess)
+                return;
+
+            // The command failed, let's notify the user that something happened.
+
+            if (result.Error == CommandError.Exception)
+                await context.Channel.SendMessageAsync("Something broke!");
+            else
+            {
+                await context.Message.AddReactionAsync(new Emoji("❌"));
+            }
+        }
+    }
+}
